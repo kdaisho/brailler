4d9f2cb3343b1a4c7fcfc6c9c519f4f20ce20b78
Daisho <daishokomiyama@gmail.com>
2017-07-30 09:47:24 -0400

    Rewrote event to occur only when the last key was released, not the first key.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,10 @@ export class AppComponent {
 	map;
 	say;
 
+	keyUpCount = 0;
+	keyCount = 0;
+	isRightKey = false;
+
 	rows = [1];
 
 	constructor(private winRef: WindowRef) {
@@ -39,9 +43,29 @@ export class AppComponent {
 		this.map = [];
 		this.map[event.keyCode] = event.type == 'keyup';
 		this.sayKeyCode(this.counter);
-		this.checkCounter();
-		this.clearDots(this.counter);
-		this.allFalse(this.counter);
+		// this.checkCounter();
+		this.keyUpCount++;
+		if(this.isRightKey && (this.keyUpCount == this.keyCount)) {
+			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			console.log("last key up");
+			this.keyUpCount = 0;
+			this.addCounter(1);
+			this.clearDots(this.counter);
+			this.allFalse(this.counter);
+			this.checkCounter();
+			this.isRightKey = false;
+			// this.keyUpCount = 0;
+		}
+		if(this.isRightKey == false) {
+			console.log("false key");
+			this.keyUpCount = 0;
+			// this.addCounter(1);
+			this.clearDots(this.counter);
+			this.allFalse(this.counter);
+			this.checkCounter();
+		}
+		// this.clearDots(this.counter);
+		// this.allFalse(this.counter);
 		if(this.map[32]) {
 			this.addCounter(1);
 		}
@@ -113,133 +137,185 @@ export class AppComponent {
 	sayKeyCode(x) {
 		if(this.items[x].dot1 && !(this.items[x].dot2 || this.items[x].dot3 || this.items[x].dot4 || this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 'a';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 1;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot2 && !(this.items[x].dot3 || this.items[x].dot4 || this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 'b';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 2;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot4 && !(this.items[x].dot2 || this.items[x].dot3 || this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 'c';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 2;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot4 && this.items[x].dot5 && !(this.items[x].dot2 || this.items[x].dot3 ||this.items[x].dot6)) {
 			this.say.text = 'd';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 3;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot5 && !(this.items[x].dot2 || this.items[x].dot3 || this.items[x].dot4 || this.items[x].dot6)) {
 			this.say.text = 'e';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 2;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot2 && this.items[x].dot4 && !(this.items[x].dot3 || this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 'f';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 3;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot2 && this.items[x].dot4 && this.items[x].dot5 && !(this.items[x].dot3 || this.items[x].dot6)) {
 			this.say.text = 'g';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 4;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot2 && this.items[x].dot5 && !(this.items[x].dot3 || this.items[x].dot4 || this.items[x].dot6)) {
 			this.say.text = 'h';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 3;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot2 && this.items[x].dot4 && !(this.items[x].dot1 || this.items[x].dot3 || this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 'i';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 2;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot2 && this.items[x].dot4 && this.items[x].dot5 && !(this.items[x].dot1 || this.items[x].dot3 || this.items[x].dot6)) {
 			this.say.text = 'j';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 3;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot3 && !(this.items[x].dot2 || this.items[x].dot4 || this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 'k';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 2;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot2 && this.items[x].dot3 && !(this.items[x].dot4 || this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 'l';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 3;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot3 && this.items[x].dot4 && !(this.items[x].dot2 || this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 'm';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 3;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot3 && this.items[x].dot4 && this.items[x].dot5 && !(this.items[x].dot2 || this.items[x].dot6)) {
 			this.say.text = 'n';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 4;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot3 && this.items[x].dot5 && !(this.items[x].dot2 || this.items[x].dot4 ||  this.items[x].dot6)) {
 			this.say.text = 'o';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 3;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot2 && this.items[x].dot3 && this.items[x].dot4 && !(this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 'p';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 4;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot2 && this.items[x].dot3 && this.items[x].dot4 && this.items[x].dot5 && !this.items[x].dot6) {
 			this.say.text = 'q';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 5;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot2 && this.items[x].dot3 && this.items[x].dot5 && !(this.items[x].dot4 || this.items[x].dot6)) {
 			this.say.text = 'r';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 4;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot2 && this.items[x].dot3 && this.items[x].dot4 && !(this.items[x].dot1 || this.items[x].dot5 || this.items[x].dot6)) {
 			this.say.text = 's';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 3;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot2 && this.items[x].dot3 && this.items[x].dot4 && this.items[x].dot5 && !(this.items[x].dot1 || this.items[x].dot6)) {
 			this.say.text = 't';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 4;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot3 && this.items[x].dot6 && !(this.items[x].dot2 || this.items[x].dot4 || this.items[x].dot5)) {
 			this.say.text = 'u';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 3;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot2 && this.items[x].dot3 && this.items[x].dot6 && !(this.items[x].dot4 || this.items[x].dot5)) {
 			this.say.text = 'v';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 4;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot2 && this.items[x].dot4 && this.items[x].dot5 && this.items[x].dot6 && !(this.items[x].dot1 || this.items[x].dot3)) {
 			this.say.text = 'w';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 4;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot3 && this.items[x].dot4 && this.items[x].dot6 && !(this.items[x].dot2 || this.items[x].dot5)) {
 			this.say.text = 'x';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 4;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot3 && this.items[x].dot4 && this.items[x].dot5 && this.items[x].dot6 && !this.items[x].dot2) {
 			this.say.text = 'y';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 5;
+			this.isRightKey = true;
 		}
 		if(this.items[x].dot1 && this.items[x].dot3 && this.items[x].dot5 && this.items[x].dot6 && !(this.items[x].dot2 || this.items[x].dot4)) {
 			this.say.text = 'z';
-			this.addCounter(1);
-			this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			// this.addCounter(1);
+			// this.winRef.nativeWindow.speechSynthesis.speak(this.say);
+			this.keyCount = 4;
+			this.isRightKey = true;
 		}
 	}
 }
